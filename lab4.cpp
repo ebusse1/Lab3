de1ba5d0984cf2028828252d4e77b4752d21ec6d
ebusse1 <ebusse1@umbc.edu>
2026-02-27 14:45:37 +0000

    Made it work

--- lab4.cpp
+++ lab4.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdio>
+#include <string>
 
 using namespace std; 
 
@@ -11,12 +12,16 @@ int main(){
     int data_array[100];
     int size = 0;
     int count = 0;
+    string file_name;
+
+    //Request file name from user
+    cout << "Enter file name: ";
+    cin >> file_name;
 
     //Open the file
-    FILE * my_file = fopen("data.txt", "r");
+    FILE * my_file = fopen(file_name.c_str(), "r");
 
     fscanf(my_file, "%d", &size); // Load the first number into size
-
 
     //Count the ints in the file, and load them in
      while (fscanf(my_file, "%d", &data_array[count]) == 1 && count < size) {
@@ -24,6 +29,6 @@ int main(){
     }
 
     find_sum(data_array, count);
-    
+
     return 0;
 }
